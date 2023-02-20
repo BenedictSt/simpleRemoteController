b40d940711f883a4f16fc7fdf916edf488927e59
Benedict <52418145+BenedictSt@users.noreply.github.com>
2023-02-20 23:15:57 +0100

    erste einfache version

--- AnnaForumController/ContentView.swift
+++ AnnaForumController/ContentView.swift
@@ -1,19 +1,48 @@
 //
 //  ContentView.swift
-//  
+//
 //
 //  Created by Benedict on 20.02.23.
 //
 
 import SwiftUI
+import OSCKit
+let oscClient = OSCClient()
 
 struct ContentView: View {
 	var body: some View {
-		VStack {
+		VStack(spacing: 30) {
 			Image(systemName: "globe")
 				.imageScale(.large)
 				.foregroundColor(.accentColor)
-			Text("Hello, world!")
+			Button(action: {
+
+//				DispatchQueue.main.asyncAfter(deadline: .now() + 10, execute: {
+					let msg = OSCMessage("/a", values: ["a"])
+						try? oscClient.send(msg, to: "10.0.0.37", port: 53000)
+						print("has send")
+
+//				try? oscClient.send(
+//					.message("/some/address/methodB", values: ["Test string", 123]),
+//					to: "10.0.0.37", // IP address or hostname
+//					port: 53000 // standard OSC port but can be changed
+//				)
+			}) {
+				Text("Test")
+			}
+
+
+			Button(action: {
+
+			}) {
+				Text("2")
+			}
+
+		Button(action: {
+			LocalNetworkPermissionService().triggerDialog()
+		}) {
+			Text("access")
+		}
 		}
 		.padding()
 	}
