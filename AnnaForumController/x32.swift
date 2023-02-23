d8eac9b24f82354fd14a259b4a6c117400827c65
Benedict <52418145+BenedictSt@users.noreply.github.com>
2023-02-23 19:00:27 +0100

    Add channel class and set setters

--- AnnaForumController/x32.swift
+++ AnnaForumController/x32.swift
@@ -14,18 +14,51 @@ enum ConnectionState {
 class X32: ObservableObject{
 	let connection: OSCConnection
 
-	class Channel: ObservableObject {
+	class Channel {
 		let name: String
+		unowned let x32: X32
 		let id: String
-		@Published var muted: Bool
-		@Published var faderValue: Float
+		private (set) var muted: Bool
+		private (set) var faderValue: Float
+		var hash: Int {
+			var hasher = Hasher()
+			hasher.combine(id)
+			hasher.combine(muted)
+			hasher.combine(faderValue)
+			return hasher.finalize()
+		}
 
-		init(name: String, id: String) {
+		init(x32: X32, name: String, id: String) {
+			self.x32 = x32
 			self.name = name
 			self.id = id
 			self.muted = true
 			self.faderValue = 0.75
 		}
+
+		func fetch() {
+			
+		}
+
+		func setMuted(_ m: Bool) {
+			let msg = OSCMessage("/ch/\(id)/mix/on", values: [muted ? 0 : 1])
+			x32.send(msg)
+		}
+
+		func setFader(_ f: Float) {
+			if f < 0 || f > 1 {
+				print("error setting fader: out of bounds: \(f)")
+			}
+
+			let msg = OSCMessage("/ch/\(id)/mix/fader", values: [f])
+			x32.send(msg)
+		}
+	}
+
+	var hash: Int {
+		var hasher = Hasher()
+		channels.forEach({hasher.combine($0.hash)})
+		return hasher.finalize()
 	}
 
 	@Published var updateId = UUID()
@@ -37,11 +70,25 @@ class X32: ObservableObject{
 	init() {
 		connection = OSCConnection(port: 9000)
 		state = .disconnected
+		channels = []
 		channels = [
-			Channel(name: "Mic 1 (blau)", id: "01"),
-			Channel(name: "Mic 2 (ohne)", id: "02"),
-			Channel(name: "Headset", id: "04"),
-			Channel(name: "Apple TV", id: "15")
+			Channel(x32: self, name: "Mic 1 (blau)", id: "01"),
+			Channel(x32: self, name: "Mic 2 (ohne)", id: "02"),
+			Channel(x32: self, name: "Headset", id: "04"),
+			Channel(x32: self, name: "Apple TV", id: "15")
 		]
+	}
+
+	func send(_ msg: OSCMessage) {
+		try? x32OSC.send(msg, to: "127.0.0.1", port: 10023)
+	}
+
+
+	func fetch() {
+		let beforeHash = hash
+		channels.forEach({$0.fetch()})
+		if beforeHash != hash {
+			updateId = UUID()
+		}
 	}
 }
