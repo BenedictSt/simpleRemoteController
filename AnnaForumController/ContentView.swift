12e3597a8f57aaab107911b7048066bd82a23789
Benedict <52418145+BenedictSt@users.noreply.github.com>
2023-02-25 21:11:15 +0100

    disable view, wenn nicht verbundne

--- AnnaForumController/ContentView.swift
+++ AnnaForumController/ContentView.swift
@@ -6,11 +6,20 @@
 //
 
 import SwiftUI
+import Stripes
 
 struct ContentView: View {
 	@State var showSettings = false
 
-	@ObservedObject var x32 = X32()
+	@ObservedObject var x32: X32
+	@ObservedObject var status: X32.Status
+
+	init() {
+		self.showSettings = false
+		let x32Tmp = X32()
+		self.x32 = x32Tmp
+		self.status = x32Tmp.status
+	}
 
 	var body: some View {
 		VStack(spacing: 30) {
@@ -30,6 +39,28 @@ struct ContentView: View {
 			ConnectionStatusView(x32: x32)
 
 			SoundView(x32: x32)
+				.disabled(!status.connected)
+				.overlay {
+					if !status.connected {
+						ZStack {
+							Stripes(config: StripesConfig(background: .black,
+														  foreground: Color.white.opacity(0.2),
+														  degrees: 45,
+														  barWidth: 5,
+														  barSpacing: 5)).opacity(0.5)
+								.cornerRadius(10)
+							VStack {
+								Image(systemName: "bolt.horizontal.fill")
+									.font(.largeTitle)
+									.foregroundColor(.white)
+								Text("Nicht Verbunden")
+									.font(.title.bold())
+									.foregroundColor(.white)
+							}
+						}
+					}
+				}
+
 			Spacer()
 		}
 		.padding()
